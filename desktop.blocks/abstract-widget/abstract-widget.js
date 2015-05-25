d07371435eaae0fbee68e277efe925655a5db3f9
andychups <andychups@ya.ru>
2015-05-26 00:00:55 +0300

    Новый блок

---
+++ desktop.blocks/abstract-widget/abstract-widget.js
@@ -0,0 +1,24 @@
+modules.define('abstract-widget', ['i-bem__dom', 'app'], function (provide, BEMDOM, app) {
+    provide(BEMDOM.decl(this.name,
+        {
+            onSetMod: {
+                'js': {
+                    'inited': function () {
+                        this.collection = app.getData('abstractCollection');
+                        this.listeners();
+                        this.render();
+                    }
+                }
+            },
+
+            listeners: function () {
+                this.collection.on('change', this.render);
+            },
+
+            render: function () {
+                this.collection.forEach(function (item) {
+                    console.log(item);
+                });
+            }
+        }));
+});
