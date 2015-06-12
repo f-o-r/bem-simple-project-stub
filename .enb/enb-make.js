501e7720f8bd03d7a436a807582e5d22536cf0b7
andychups <andychups@ya.ru>
2015-06-12 11:16:12 +0300

    Добавил вычитание

--- .enb/enb-make.js
+++ .enb/enb-make.js
@@ -1,15 +1,23 @@
+var path = require('path');
+var bundlesPath = 'desktop.bundles';
+var commonName = 'common';
+var commonPath = path.join(bundlesPath, commonName);
+var commonYateObjPath = path.resolve(path.join(bundlesPath, commonName));
+
+commonYateObjPath = [commonYateObjPath, 'common.yate.obj'].join('/');
+
 module.exports = function(config) {
     config.nodes('desktop.bundles/*');
-
 
     config.nodeMask(/desktop\.bundles\/.*/, function(nodeConfig) {
         nodeConfig.addTechs([
             new (require('enb/techs/file-provider'))({ target: '?.bemdecl.js' }),
             new (require('enb/techs/levels'))({ levels: getLevels(config) }),
-            new (require('enb/techs/deps-old'))(),
             new (require('enb/techs/files'))(),
             new (require('enb/techs/js'))(),
-            new (require('./techs/yate.js'))(),
+            new (require('./techs/yate.js'))({
+                'commonYateObjPath': commonYateObjPath
+            }),
             new (require('enb/techs/vanilla-js'))(),
             new (require('enb-stylus/techs/css-stylus-with-nib'))({
                 'target': '?.css',
@@ -19,6 +27,18 @@ module.exports = function(config) {
             }),
             new (require('enb/techs/file-merge'))({ sources: [ '?.vanilla.js', '?.js', '?.yate.js' ], target: '?.merge.js' })
         ]);
+
+        if (nodeConfig.getPath() === commonPath) {
+            nodeConfig.addTechs([
+                new (require('enb/techs/deps-old'))()
+            ]);
+        } else {
+            nodeConfig.addTechs([
+                new (require('enb/techs/deps-old'))({ depsTarget: '?.big.deps.js' }),
+                new (require('enb/techs/deps-provider'))({ sourceNodePath: commonPath, depsTarget: commonName+'.deps.js' }),
+                new (require('enb/techs/deps-subtract'))({ subtractWhatTarget: commonName+'.deps.js', subtractFromTarget: '?.big.deps.js', depsTarget: '?.deps.js' })
+            ]);
+        }
 
         nodeConfig.addTargets([
             '?.vanilla.js', '?.js', '?.merge.js', '_?.js', '?.css', '_?.css', '?.yate.js'
