aa34222ae29e020e6036801a9390b3e931f1ddd3
andychups <andychups@ya.ru>
2015-06-12 16:44:43 +0300

    Рефакторинг технологии сборки

--- .enb/techs/yate.js
+++ .enb/techs/yate.js
@@ -2,6 +2,82 @@ const YATE_EXTENSION = 'yate';
 
 var fs = require('fs');
 var yate = require('yate');
+var methods = {};
+
+function builder (sourceFiles) {
+    var node;
+
+    try {
+        var yate = require('yate');
+    } catch (e) {
+        throw new Error('The technology "yate" cannot be executed because the npm module "yate" was not found.');
+    }
+
+    node = this.node;
+
+    this._targetName = node.unmaskTargetName('?');
+    this._targetPath = node.resolvePath('');
+    this._targetYateFilePath = this._targetPath + this._targetName + '.' + YATE_EXTENSION;
+    this._sourceFiles = sourceFiles;
+
+    if (!sourceFiles.length) {
+        return '';
+    }
+
+    this.prepareYateTemplate();
+    this.importCommon();
+
+    return this.compile();
+}
+
+methods.prepareYateTemplate = function () {
+    var targetDepsYateSource = 'module "'+this._targetName+'"\n\n';
+
+    // Импортим общий common
+    if (!this._isCommon && this._commonYateObjPath && fs.existsSync(this._commonYateObjPath)) {
+        targetDepsYateSource += 'import "common"\n\n';
+    }
+
+    // Собираем зависимости
+    targetDepsYateSource += this._sourceFiles.map(function(file) {
+        return 'include "'+file.fullname+'"';
+    }).join('\n');
+
+    // Сохраняем зависимости в файл на жесткий диск, для последующей компиляции
+    fs.writeFileSync(this._targetYateFilePath, targetDepsYateSource, 'utf-8');
+};
+
+methods.importCommon = function () {
+    var commonYateObj;
+
+    // Если это common бандл, то для него него нужно делать импорт
+    if (this._isCommon) {
+        return;
+    }
+
+    // Добавляем импорты, аналогично этому: https://github.com/pasaran/yate/blob/master/yate#L53
+    if (this._commonYateObjPath && fs.existsSync(this._commonYateObjPath)) {
+        yate.modules = {};
+        commonYateObj = JSON.parse(fs.readFileSync(this._commonYateObjPath));
+        yate.modules[commonYateObj.name] = commonYateObj;
+    } else {
+        this.node.getLogger().logWarningAction('warning', targetName+'.yate.js', 'Не найден или не задан common бандл');
+    }
+};
+
+methods.compile = function () {
+    var resultYateJsSource;
+
+    try {
+        resultYateJsSource = this._prependJs;
+        resultYateJsSource += yate.compile(this._targetYateFilePath).js;
+        resultYateJsSource += this._appendJs;
+    } catch (error) {
+        throw new Error('The technology "yate" throw compile error. '+error);
+    }
+
+    return resultYateJsSource;
+};
 
 module.exports = require('enb/lib/build-flow').create()
     .name('yate')
@@ -10,63 +86,8 @@ module.exports = require('enb/lib/build-flow').create()
     .defineOption('verbose', false)
     .defineOption('commonYateObjPath', false)
     .defineOption('isCommon', false)
+    .defineOption('prependJs', '')
+    .defineOption('appendJs', '')
     .builder(builder)
+    .methods(methods)
     .createTech();
-
-function builder (sourceFiles) {
-    try {
-        var yate = require('yate');
-    } catch (e) {
-        throw new Error('The technology "yate" cannot be executed because the npm module "yate" was not found.');
-    }
-
-    var node = this.node;
-    var targetName = node.unmaskTargetName('?');
-    var targetPath = node.resolvePath('');
-    var targetYateFilePath = targetPath + targetName + '.' + YATE_EXTENSION;
-    var targetDepsYateSource;
-    var resultYateFilePath = targetYateFilePath;
-    var resultYateJsSource = '';
-    var commonYateObjPath = this._commonYateObjPath;
-    var commonYateObj;
-    var hasCommonYate = commonYateObjPath && fs.existsSync(commonYateObjPath);
-
-    if (!sourceFiles.length) {
-        return resultYateJsSource;
-    }
-
-    // Готовим Yate к компиляции
-    targetDepsYateSource = 'module "'+targetName+'"\n\n';
-
-    // Импортим общий common
-    if (!this._isCommon && hasCommonYate) {
-        targetDepsYateSource += 'import "common"\n\n';
-    }
-
-    // Собираем зависимости
-    targetDepsYateSource += sourceFiles.map(function(file) {
-        return 'include "'+file.fullname+'"';
-    }).join('\n');
-
-    // Сохраняем зависимости в файл на жесткий диск, для последующей компиляции
-    fs.writeFileSync(targetYateFilePath, targetDepsYateSource, 'utf-8');
-
-    // Добавляем импорты, аналогично этому: https://github.com/pasaran/yate/blob/master/yate#L53
-    if (hasCommonYate) {
-        yate.modules = {};
-        commonYateObj = JSON.parse(fs.readFileSync(commonYateObjPath));
-        yate.modules[commonYateObj.name] = commonYateObj;
-    } else {
-        this.node.getLogger().logWarningAction('warning', targetName+'.yate.js', 'Не найден или не задан common бандл');
-    }
-
-    try {
-        resultYateJsSource = 'modules.require("yate", function (yr) {';
-        resultYateJsSource += yate.compile(resultYateFilePath).js
-        resultYateJsSource += '});';
-    } catch (error) {
-        throw new Error('The technology "yate" throw compile error. '+error);
-    }
-
-    return resultYateJsSource;
-}
